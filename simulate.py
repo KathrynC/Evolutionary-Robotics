d57f5342593e15543245c8be9f66f17655e73272
Kathryn Cramer <kathryn.cramer@gmail.com>
2026-01-11 13:32:04 -0500

    Clean simulate loop (Ctrl-C to quit, print 1 Hz)

--- simulate.py
+++ simulate.py
@@ -1,13 +1,18 @@
 import time
 import pybullet as p
 
-cid = p.connect(p.GUI)
+p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 
-for _ in range(240):
-    p.stepSimulation()
-    time.sleep(1/60)
-
-input("Press Enter to quit...")
-p.disconnect()
-print("simulate.py OK")
+i = 0
+try:
+    while True:
+        p.stepSimulation()
+        time.sleep(1/60)
+        i += 1
+        if i % 60 == 0:
+            print(i)
+except KeyboardInterrupt:
+    print("\nStopped with Ctrl-C")
+finally:
+    p.disconnect()
