1c48d52682e7c1b939d9189bbe4b61c8376633db
Kathryn Cramer <kathryn.cramer@gmail.com>
2026-01-11 13:03:21 -0500

    Add simulation starter script

---
+++ simulation/simulate.py
@@ -0,0 +1,12 @@
+import time
+import pybullet as p
+
+cid = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+
+for _ in range(240):
+    p.stepSimulation()
+    time.sleep(1/60)
+
+p.disconnect()
+print("simulate.py OK")
